8b1c62f4bde577e401e1c7963d8a84c6ce553e89
Pedro Cunial <pedrocunial@gmail.com>
2016-11-14 15:42:03 -0200

    TEST: writing cli

--- api/app.py
+++ api/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/new_image", methods=["GET"])
+@app.route("/new_image")
 def new_image():
     """
     Executes script for processing new users
     data
     """
     # TODO
+    os.system("touch /opt/data/shrek.txt")
+    return "DONE", 200
 
 
 if __name__ == "__main__":
