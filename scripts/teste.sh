eb3098fc7d9ee95a7e794523a48699db0a8f7a47
Pedro Cunial <pedrocunial@gmail.com>
2016-11-14 16:29:58 -0200

    BUGFIX: teste.sh

--- scripts/teste.sh
+++ scripts/teste.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Checks if a given user is allowed to enter
-$OPT=/opt/data
+OPT=/opt/data
 
 $OPT/openface/classifier.py infer $OPT/features/classifier.pkl $OPT/examples/compare.jpeg > result.txt
